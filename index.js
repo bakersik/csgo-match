013187624739185ccc6416bc367798bc872cef78
bakersik <szypulski.adam99@gmail.com>
2023-02-22 20:32:22 +0100

    added basic CRUD for tournaments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const Tournament = require('./models/tournament')
 
 mongoose.connect('mongodb://localhost:27017/csgo-match')
@@ -16,14 +17,48 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/new_tournament', async (req, res) => {
-    const tournament = new Tournament({ name: 'TestowyTurniej', description: 'super turniej' })
+app.get('/tournaments', async (req, res) => {
+    const tournaments = await Tournament.find({})
+    res.render('tournaments/index', { tournaments })
+})
+
+app.get('/tournaments/new', (req, res) => {
+    res.render('tournaments/new')
+})
+
+app.post('/tournaments', async (req, res) => {
+    const tournament = new Tournament(req.body.tournament)
     await tournament.save()
-    res.send(tournament)
+    res.redirect(`/tournaments/${tournament._id}`)
+})
+
+app.get('/tournaments/:id', async (req, res) => {
+    const tournament = await Tournament.findById(req.params.id)
+    res.render('tournaments/show', { tournament })
+})
+
+app.get('/tournaments/:id/edit', async (req, res) => {
+    const tournament = await Tournament.findById(req.params.id)
+    res.render('tournaments/edit', { tournament })
+})
+
+app.put('/tournaments/:id', async (req, res) => {
+    const { id } = req.params
+    const tournament = await Tournament.findByIdAndUpdate(id, { ...req.body.tournament })
+    res.redirect(`/tournaments/${tournament._id}`)
+})
+
+app.delete('/tournaments/:id', async (req, res) => {
+    const { id } = req.params
+    await Tournament.findByIdAndDelete(id)
+    res.redirect('/tournaments')
 })
 
 app.listen(3000, () => {
--- seeds/index.js
+++ seeds/index.js
@@ -8,11 +8,34 @@ db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
     console.log('Database connected')
 })
-
-const seedDB = async () => {
-    await Tournament.deleteMany({})
-    const t = new Tournament({ name: 'csgo50' })
-    await t.save()
-}
-
-seedDB()
+//Tournament.deleteMany({})
+Tournament.insertMany([
+    {
+        name: 'Turniej o puchar prezydenta Ciechanowa',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, rerum porro? Repellendus quia cumque blanditiis necessitatibus corporis alias dolore. Repudiandae error voluptatibus libero amet. Ducimus, odit quo. Id, distinctio facere.',
+        start_at: '2023-05-31',
+        prize: 5000
+    },
+    {
+        name: 'Ogolnopolskie zawody w wyciaganiu penisa z wody',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, rerum porro? Repellendus quia cumque blanditiis necessitatibus corporis alias dolore. Repudiandae error voluptatibus libero amet. Ducimus, odit quo. Id, distinctio facere.',
+        start_at: '2023-02-13',
+        prize: 6699
+    },
+    {
+        name: 'Dreamhack Poland edition',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, rerum porro? Repellendus quia cumque blanditiis necessitatibus corporis alias dolore. Repudiandae error voluptatibus libero amet. Ducimus, odit quo. Id, distinctio facere.',
+        start_at: '2023-05-31',
+        prize: 10000
+    },
+    {
+        name: 'IEM Katowice w Spodku',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, rerum porro? Repellendus quia cumque blanditiis necessitatibus corporis alias dolore. Repudiandae error voluptatibus libero amet. Ducimus, odit quo. Id, distinctio facere.',
+        start_at: '2023-04-30',
+        prize: 15000
+    }
+]).then(function () {
+    console.log("Data inserted")
+}).catch(function (error) {
+    console.log(error)
+});
