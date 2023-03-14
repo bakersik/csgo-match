d79fdc248e5136daab6e127c398a1a4bae04031c
bakersik <szypulski.adam99@gmail.com>
2023-03-15 00:55:32 +0100

    added SQL Injection and XSS protection

--- schemas.js
+++ schemas.js
@@ -1,9 +1,32 @@
-const Joi = require('joi')
+const BaseJoi = require('joi')
+const sanitizeHtml = require('sanitize-html')
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                })
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean
+            }
+        }
+    }
+})
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.tournamentSchema = Joi.object({
     tournament: Joi.object({
-        name: Joi.string().required(),
-        description: Joi.string(),
+        name: Joi.string().required().escapeHTML(),
+        description: Joi.string().escapeHTML(),
         start_at: Joi.date().required(),
         end_at: Joi.date(),
         prize: Joi.number().required().min(0)
@@ -12,17 +35,17 @@ module.exports.tournamentSchema = Joi.object({
 
 module.exports.teamSchema = Joi.object({
     team: Joi.object({
-        name: Joi.string().required(),
-        avatar_url: Joi.string().allow(''),
-        steamgroup_url: Joi.string().allow(''),
-        description: Joi.string().allow(''),
+        name: Joi.string().required().escapeHTML(),
+        avatar_url: Joi.string().allow('').escapeHTML(),
+        steamgroup_url: Joi.string().allow('').escapeHTML(),
+        description: Joi.string().allow('').escapeHTML(),
         matches_won: Joi.number(),
         matches_lost: Joi.number(),
-        member1: Joi.string().required(),
-        member2: Joi.string().required(),
-        member3: Joi.string().required(),
-        member4: Joi.string().required(),
-        member5: Joi.string().required(),
+        member1: Joi.string().required().escapeHTML(),
+        member2: Joi.string().required().escapeHTML(),
+        member3: Joi.string().required().escapeHTML(),
+        member4: Joi.string().required().escapeHTML(),
+        member5: Joi.string().required().escapeHTML(),
     }).required()
 })
 
